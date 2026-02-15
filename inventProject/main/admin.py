bc6c43cb13d9fd2b4f0217646b1a77a3e982ba49
Andrey Monastyrskiy <a.monastyrskiy@yandex.ru>
2026-02-15 13:07:13 +0300

    Import and Export Fixed

--- inventProject/main/admin.py
+++ inventProject/main/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from .models import Equipment, EquipmentStatus, EquipmentType, Office
-from import_export import resources
+from .models import Equipment, EquipmentStatus, EquipmentType, Office 
+from import_export import resources, fields
 from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import ForeignKeyWidget
 
 class EquipmentResource(resources.ModelResource):
+    equipment_type = fields.Field(
+        column_name='equipment_type',
+        attribute='equipment_type',
+        widget=ForeignKeyWidget(EquipmentType, 'name')
+    )
+    office = fields.Field(
+        column_name='office',
+        attribute='office',
+        widget=ForeignKeyWidget(Office, 'name')
+    )
+    status = fields.Field(
+        column_name='status',
+        attribute='status',
+        widget=ForeignKeyWidget(EquipmentStatus, 'name')
+    )
     class Meta:
         model = Equipment
+
 
 # Register your models here.
 @admin.register(Equipment)
@@ -13,9 +30,6 @@ class EquipmentAdmin(ImportExportModelAdmin):
     resource_classes = [EquipmentResource]
     list_display = ('name', 'accounting_name', 'serial_number', 'inventory_number', 'equipment_type', 'office', 'status', 'description')
     list_filter = ('equipment_type', 'office', 'status')
-#class EquipmentAdmin(admin.ModelAdmin):
-#    list_display = ('name', 'accounting_name', 'serial_number', 'inventory_number', 'equipment_type', 'office', 'status', 'description')
-#    list_filter = ('equipment_type', 'office', 'status')
 
 
 @admin.register(EquipmentStatus)
